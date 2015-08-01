c33631a77da3f5fcb341ddc9a868136d902b6333
Austin Keeley <austin.keeley@gmail.com>
2015-08-01 18:51:52 -0400

    MERGING THIS MOFO

--- thinktankdata/landing/urls.py
+++ thinktankdata/landing/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, url
-from landing.views import *
+from landing.views import index
 
 urlpatterns = [
-    url(r'^$', FrontPageList.as_view(), name='index'),
+    url(r'^$', index, name='index')
 #    url(r'^season/(?P<slug>[a-zA-Z0-9_-]+).html$', WeekDetail.as_view(), name='week'),
 #    url(r'^(?P<slug>[a-zA-Z0-9_-]+).html$', CollectionTemplateView.as_view(), name='collection'),
 #    url(r'^$', IndexView.as_view(), name='index'),
