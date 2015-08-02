03d1494db4b5fcc07bcba1a0e89ac5209e9bc29f
Bobby Mozumder <mozumder@futureclaw.com>
2015-08-02 02:50:09 -0400

    Undoing URL fix for server

--- thinktankdata/thinktankdata/urls.py
+++ thinktankdata/thinktankdata/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^thinkdata/admin/', include(admin.site.urls)),
-    url(r'^thinkdata/$', include('landing.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('landing.urls')),
 ]
